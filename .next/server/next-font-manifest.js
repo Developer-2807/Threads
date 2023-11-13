2941e0b9d75af728c1a141a5b75cdc497a20c249
Sabari-A <sabari.2k02@gmail.com>
2023-11-13 23:34:57 +0530

    First deployment

---
+++ .next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/home/sabari/Programming/WebDeveloping/My Github/threads/app/(root)/layout\":[\"static/media/c9a5bc6a7c948fb0-s.p.woff2\"],\"/home/sabari/Programming/WebDeveloping/My Github/threads/app/(auth)/layout\":[\"static/media/c9a5bc6a7c948fb0-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
